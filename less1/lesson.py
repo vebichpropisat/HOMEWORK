c9491cbe3385ab48371d7c914ccf6806ef1353c1
Zakhar <zzz.krav@gmail.com>
2022-10-28 19:13:22 +0300

    first commit

---
+++ less1/lesson.py
@@ -0,0 +1,21 @@
+import random
+
+def main():
+    while True:
+        random_number = random.randint(0, 10)
+        user_number = int(input("Введите число "))
+
+        if random_number == user_number:
+            print("Вы выйграли")
+        else:
+            print("Вы проиграли")
+
+        answer = input("Играть еще? ")
+
+        if answer == "Нет":
+            break
+        elif answer == "Да":
+            continue
+
+if __name__ == "__main__":
+    main()
